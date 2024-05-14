119ee978747b70a33248fb53de3260846bb904e5
Abhijeet2370 <119788761+Abhijeet2370@users.noreply.github.com>
2024-05-15 00:40:35 +0530

    Add files via upload

---
+++ admin/includes/admin_navigation.php
@@ -0,0 +1,104 @@
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">Blog Admin</a>
+            </div>
+            <!-- Top Menu Items -->
+            <ul class="nav navbar-right top-nav">
+            <li><a href="../index.php">Home</a></li>
+
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $_SESSION['username'] ?><b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li class="divider"></li>
+                        <li>
+                            <a href='index.php?logout={$_SESSION["username"]}'><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+                        </li>
+
+                                <?php
+                                    if(isset($_GET['logout'])){
+                                        session_destroy();
+                                        header("Location: ../index.php");
+                                    }
+                                ?>
+                    </ul>
+                </li>
+            </ul>
+
+
+
+
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li class="active">
+                        <a href="index.php" class="active"><i class="fa fa-fw fa-dashboard active"></i> Dashboard</a>
+                    </li>
+
+                    <li>
+                        <a href="./profile.php"><i class="fa fa-fw fa-id-card-alt"><strong class="fas fa-male"></strong></i> Profile</a>
+                    </li>
+
+                    <li>
+                        <a href="./categories.php"><i class="fa fa-fw fa-arrows-v"></i> Categories</a>
+                    </li>
+
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-paste-v"><i class="fas fa-paste"></i></i></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="posts_dropdown" class="collapse">
+                            <li>
+                                <a href="./posts.php">View all Posts</a>
+                            </li>
+                            <li>
+                                <a href="./new_post.php">Add Posts</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    
+
+                    <li class="">
+                        <a href="./comments.php"><i class="fa fa-fw fa-comments"></i> Comments</a>
+                    </li>
+
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdn"><i class="fa fa-fw fa-male-v"><p class="fas fa-users"></p></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="posts_dropdn" class="collapse">
+                            <li>
+                                <a href="./users.php">View All Users</a>
+                            </li>
+                            <li>
+                                <a href="./new_user.php">Add Users</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdn1"><i class="fa fa-fw fa-tasks"></i> Events <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="posts_dropdn1" class="collapse">
+                            <li>
+                                <a href="./events.php">View All Events</a>
+                            </li>
+                            <li>
+                                <a href="./add_event.php">Add Event</a>
+                            </li>
+                        </ul>
+                    </li>
+
+
+
+
+                </ul>
+            </div>
+
+
+
+            <!-- /.navbar-collapse -->
+        </nav>
